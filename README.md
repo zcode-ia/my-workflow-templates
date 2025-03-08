9b2b044e7dda7a374029e67fdc1e6cefa8c24a19
ZCode <zcode.ia@gmail.com>
2025-03-08 09:45:27 -0300

    add dependencies and comments

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # my-workflow-templates
 My workflow templates repository
 
+## Repository dependencies
+Please, install the following dependencies before setup this repository
+
+* pre-commit
+
 ## Repository setup
 Run the follow command to setup the repository
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 enable-pre-commit() {
+    # Check if pre-commit is installed
     if command -v pre-commit &> /dev/null; then
+        # Install pre-commit hooks
         pre-commit install
     else
+        # Print error message if pre-commit is not found
         echo "Error: pre-commit not found! Please, install it."
         exit 1
     fi
